f2aa17e3487444adbdfc73369f6bb18fbf74f99a
Daniel Nagai <daniel@shop-office.net>
2020-07-29 03:22:50 +0000

    initial commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div style={{textAlign:'center', color:'#fff'}}>
+        <div style={{textAlign:'center',fontSize:'1.8rem', color:'#fff', position:'relative',marginTop:'15%'}}>
             <h1>Welcome to our Homepage</h1>
         </div>
     )
