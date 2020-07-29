18854b75fe36d838931db81fffbe04d85ebb1153
Daniel Nagai <daniel@shop-office.net>
2020-07-29 03:10:09 +0000

    initial commit

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
 
-const Services = ({match}) => {
+const Services = () => {
     
     return (
         <div>
-            {""}
+            
             <h2>Our Services </h2>
         </div>
     )
